b8abecb27b1fd989157f551b5c7f8379626525ac
lowkeyhuman <iamlowkeyhuman@gmail.com>
2023-12-04 23:02:17 +0000

    Add Level with Player, but without controls

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,12 +1,17 @@
 import { OrbitControls } from "@react-three/drei"
 import Level from "../level/Level"
 import Lights from "../lights/Lights"
+import { Physics } from "@react-three/rapier"
+import Player from "../player/Player"
 
 const Experience = () => {
   return (
     <>
-      <Level />
-      <Lights />
+      <Physics debug>
+        <Level />
+        <Lights />
+        <Player /> 
+      </Physics>
 
       <OrbitControls makeDefault/>
     </>
